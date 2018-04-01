196744de66a21e1ce5329b24388c1e54d407088a
Shubham Singh <imshubhamsingh97@gmail.com>
2018-04-01 17:29:49 +0530

    flip team card

--- src/components/Dashboard/Layout/HomeLayout.vue
+++ src/components/Dashboard/Layout/HomeLayout.vue
@@ -9,9 +9,12 @@
       <!--</div>-->
       <div class="teams">
         <span class="top-text">Teams</span>
-        <div class="team-list">
-          <app-teams/>
+        <div class="team-scroll">
+          <div class="team-list">
+            <app-teams v-for="team in teams" :team="team" :key="team.short"/>
+          </div>
         </div>
+
       </div>
     </div>
 
@@ -23,9 +26,6 @@
         </div>
       </div>
     </div>
-
-
-
   </div>
 </template>
 
@@ -52,6 +52,125 @@
           { year: 2014, backgroundColor: 'linear-gradient( 135deg, #FF96F9 10%, #C32BAC 100%)', matchPlayed: 60 },
           { year: 2015, backgroundColor: 'linear-gradient( 135deg, #F761A1 10%, #8C1BAB 100%)', matchPlayed: 57 },
           { year: 2016, backgroundColor: 'linear-gradient( 135deg, #FCCF31 10%, #F55555 100%)', matchPlayed: 60 }
+        ],
+        teams: [
+          {
+            image: 'static/img/team-logo/Chennai_Super_Kings_Logo.png',
+            name: 'Chennai Super Kings',
+            photo: 'static/img/team-photo/Chennai_Super_Kings.jpg',
+            short: 'CSK',
+            width: '121px',
+            marginTop: '0px',
+            color: 'linear-gradient( 135deg, #FDEB71F1 10%, #F8D800C1 100%)'
+          },
+          {
+            image: 'static/img/team-logo/Deccan_Chargers.png',
+            name: 'Deccan Chargers',
+            photo: 'static/img/team-photo/Deccan_Chargers.jpg',
+            short: 'DC',
+            width: '100px',
+            marginTop: '0px',
+            color: 'linear-gradient( 135deg, #97ABFFF1 10%, #123597C1 100%)'
+          },
+          {
+            image: 'static/img/team-logo/Delhi_Daredevils.png',
+            name: 'Delhi Daredevils',
+            photo: 'static/img/team-photo/Delhi_Daredevils.jpg',
+            short: 'DD',
+            width: '200px',
+            marginTop: '16px',
+            color: 'linear-gradient(135deg , #e32a26 0%, #ffb199c1 100%)'
+          },
+          {
+            image: 'static/img/team-logo/Gujarat_Lions.png',
+            name: 'Gujarat Lions',
+            photo: 'static/img/team-photo/Gujarat_Lions.jpg',
+            short: 'GL',
+            width: '90px',
+            marginTop: '0px',
+            color: 'linear-gradient( 135deg, #FCCF31F1 10%, #F55555C1 100%)'
+          },
+          {
+            image: 'static/img/team-logo/Kings_XI_Punjab.png',
+            name: 'King XI Punjab',
+            photo: 'static/img/team-photo/Kings_XI_Punjab.jpg',
+            short: 'KXIP',
+            width: '94px',
+            marginTop: '0px',
+            color: 'linear-gradient(135deg, #e01111f1 10%, #945a5ac1 100%)'
+          },
+          {
+            image: 'static/img/team-logo/Kochi_Tuskers_Kerala.png',
+            name: 'Kochi Tuskers Kerala',
+            photo: 'static/img/team-photo/Kochi_Tuskers_Kerala.jpg',
+            short: 'KTK',
+            width: '127px',
+            marginTop: '11px',
+            color: 'linear-gradient( 135deg, #FF6FD8F1 10%, #3813C2C1 100%)'
+          },
+          {
+            image: 'static/img/team-logo/Kolkata_Knight_Riders.png',
+            name: 'Kolkata Knight Riders',
+            photo: 'static/img/team-photo/Kolkata_Knight_Riders.jpg',
+            short: 'KKR',
+            width: '72px',
+            marginTop: '0px',
+            color: 'linear-gradient( 135deg, #3B2667F1 10%, #BC78ECC1 100%)'
+          },
+          {
+            image: 'static/img/team-logo/Mumbai_Indians.png',
+            name: 'Mumbai Indians',
+            photo: 'static/img/team-photo/Mumbai_Indians.jpg',
+            short: 'MI',
+            width: '150px',
+            marginTop: '12px',
+            color: 'linear-gradient( 135deg, #52E5E7F1 10%, #130CB7C1 100%)'
+          },
+          {
+            image: 'static/img/team-logo/Pune_Warrior.png',
+            name: 'Pune Warrior',
+            photo: 'static/img/team-photo/Pune_Warrior.jpg',
+            short: 'PW',
+            width: '200px',
+            marginTop: '0px',
+            color: 'linear-gradient(135deg, #bdbdbdeb 10%, #000000eb 100%)'
+          },
+          {
+            image: 'static/img/team-logo/Rajasthan_Royals.png',
+            name: 'Rajasthan Royals',
+            photo: 'static/img/team-photo/Rajasthan_Royals.jpg',
+            short: 'RR',
+            width: '120px',
+            marginTop: '0px',
+            color: 'linear-gradient( 135deg, #6B73FFF1 10%, #000DFFC1 100%)'
+          },
+          {
+            image: 'static/img/team-logo/Rising_Pune_Supergiant.png',
+            name: 'Rising Pune Supergiant',
+            photo: 'static/img/team-photo/Rising_Pune_Supergiant.jpg',
+            short: 'RPS',
+            width: '205px',
+            marginTop: '23px',
+            color: 'linear-gradient( 135deg, #FF6FD8F1 10%, #3813C2C1 100%)'
+          },
+          {
+            image: 'static/img/team-logo/Royal_Challengers_Bangalore.png',
+            name: 'Royal Challenges Bangalore',
+            photo: 'static/img/team-photo/Royal_Challengers_Bangalore.jpg',
+            short: 'RCB',
+            width: '80px',
+            marginTop: '0px',
+            color: 'linear-gradient(135deg, #c9920e 10%, #f9de8cb5 100%)'
+          },
+          {
+            image: 'static/img/team-logo/Sunrisers_Hyderabad.png',
+            name: 'Sunrisers Hyderabad',
+            photo: 'static/img/team-photo/Sunrisers_Hyderabad.jpg',
+            short: 'SH',
+            width: '150px',
+            marginTop: '0px',
+            color: 'linear-gradient( 135deg, #FD6E6AF1 10%, #FFC600C1 100%)'
+          }
         ]
       };
     }
@@ -83,7 +202,7 @@
           -webkit-background-clip: text;
           -webkit-text-fill-color: transparent;
         }
-        & .team-list {
+        & .team-scroll {
           display: block;
           width: 100%;
           height: 383px;
@@ -92,6 +211,10 @@
           white-space: nowrap;
           margin-bottom: 21px;
           border-radius: 5px;
+          & .team-list{
+            overflow: hidden;
+            float: left;
+          }
         }
       }
       & .top-map{
--- src/components/UIComponents/Teams.vue
+++ src/components/UIComponents/Teams.vue
@@ -1,14 +1,21 @@
 <template>
-  <div class="team-list">
-    <div class="team-card" v-for="team in members" :style="style(team.photo)" >
-      <div class="content" :style="{'background': team.color}">
-        <div class="logo">
-          <img :src="team.image" :alt="team.name" :width="team.width" :style="{'marginTop': team.marginTop}">
-        </div>
-        <div class="text">
+  <div :class="`team-card ${team.short}-card`">
+    <div class="card">
+      <div class="team-card-front" :style="style(team.photo)" >
+        <div class="content" :style="{'background': team.color}">
+          <div class="logo">
+            <img :src="team.image" :alt="team.name" :width="team.width" :style="{'marginTop': team.marginTop}">
+          </div>
+          <div class="text">
             <div class="team-name">
               {{team.name}}
             </div>
+          </div>
+        </div>
+      </div>
+      <div class="team-card-back">
+        <div class="content" :style="{'background': team.color}">
+            <span class="team-statistic">TEAM STATISTIC</span>
         </div>
       </div>
     </div>
@@ -18,173 +25,95 @@
 <script>
   export default {
     name: 'year-card',
-    data() {
-      return {
-        title: 'Team members',
-        members: [
-          {
-            image: 'static/img/team-logo/Chennai_Super_Kings_Logo.png',
-            name: 'Chennai Super Kings',
-            photo: 'static/img/team-photo/Chennai_Super_Kings.jpg',
-            short: 'CSK',
-            width: '121px',
-            marginTop: '0px',
-            color: 'linear-gradient( 135deg, #FDEB71F1 10%, #F8D800C1 100%)'
-          },
-          {
-            image: 'static/img/team-logo/Deccan_Chargers.png',
-            name: 'Deccan Chargers',
-            photo: 'static/img/team-photo/Deccan_Chargers.jpg',
-            short: 'DC',
-            width: '100px',
-            marginTop: '0px',
-            color: 'linear-gradient( 135deg, #97ABFFF1 10%, #123597C1 100%)'
-          },
-          {
-            image: 'static/img/team-logo/Delhi_Daredevils.png',
-            name: 'Delhi Daredevils',
-            photo: 'static/img/team-photo/Delhi_Daredevils.jpg',
-            short: 'DD',
-            width: '200px',
-            marginTop: '16px',
-            color: 'linear-gradient(135deg , #e32a26 0%, #ffb199c1 100%)'
-          },
-          {
-            image: 'static/img/team-logo/Gujarat_Lions.png',
-            name: 'Gujarat Lions',
-            photo: 'static/img/team-photo/Gujarat_Lions.jpg',
-            short: 'GL',
-            width: '90px',
-            marginTop: '0px',
-            color: 'linear-gradient( 135deg, #FCCF31F1 10%, #F55555C1 100%)'
-          },
-          {
-            image: 'static/img/team-logo/Kings_XI_Punjab.png',
-            name: 'King XI Punjab',
-            photo: 'static/img/team-photo/Kings_XI_Punjab.jpg',
-            short: 'KXIP',
-            width: '94px',
-            marginTop: '0px',
-            color: 'linear-gradient(135deg, #e01111f1 10%, #945a5ac1 100%)'
-          },
-          {
-            image: 'static/img/team-logo/Kochi_Tuskers_Kerala.png',
-            name: 'Kochi Tuskers Kerala',
-            photo: 'static/img/team-photo/Kochi_Tuskers_Kerala.jpg',
-            short: 'KTK',
-            width: '127px',
-            marginTop: '11px',
-            color: 'linear-gradient( 135deg, #FF6FD8F1 10%, #3813C2C1 100%)'
-          },
-          {
-            image: 'static/img/team-logo/Kolkata_Knight_Riders.png',
-            name: 'Kolkata Knight Riders',
-            photo: 'static/img/team-photo/Kolkata_Knight_Riders.jpg',
-            short: 'KKR',
-            width: '72px',
-            marginTop: '0px',
-            color: 'linear-gradient( 135deg, #3B2667F1 10%, #BC78ECC1 100%)'
-          },
-          {
-            image: 'static/img/team-logo/Mumbai_Indians.png',
-            name: 'Mumbai Indians',
-            photo: 'static/img/team-photo/Mumbai_Indians.jpg',
-            short: 'MI',
-            width: '150px',
-            marginTop: '12px',
-            color: 'linear-gradient( 135deg, #52E5E7F1 10%, #130CB7C1 100%)'
-          },
-          {
-            image: 'static/img/team-logo/Pune_Warrior.png',
-            name: 'Pune Warrior',
-            photo: 'static/img/team-photo/Pune_Warrior.jpg',
-            short: 'PW',
-            width: '200px',
-            marginTop: '0px',
-            color: 'linear-gradient(135deg, #bdbdbdeb 10%, #000000eb 100%)'
-          },
-          {
-            image: 'static/img/team-logo/Rajasthan_Royals.png',
-            name: 'Rajasthan Royals',
-            photo: 'static/img/team-photo/Rajasthan_Royals.jpg',
-            short: 'RR',
-            width: '120px',
-            marginTop: '0px',
-            color: 'linear-gradient( 135deg, #6B73FFF1 10%, #000DFFC1 100%)'
-          },
-          {
-            image: 'static/img/team-logo/Rising_Pune_Supergiant.png',
-            name: 'Rising Pune Supergiant',
-            photo: 'static/img/team-photo/Rising_Pune_Supergiant.jpg',
-            short: 'RPS',
-            width: '205px',
-            marginTop: '23px',
-            color: 'linear-gradient( 135deg, #FF6FD8F1 10%, #3813C2C1 100%)'
-          },
-          {
-            image: 'static/img/team-logo/Royal_Challengers_Bangalore.png',
-            name: 'Royal Challenges Bangalore',
-            photo: 'static/img/team-photo/Royal_Challengers_Bangalore.jpg',
-            short: 'RCB',
-            width: '80px',
-            marginTop: '0px',
-            color: 'linear-gradient(135deg, #c9920e 10%, #f9de8cb5 100%)'
-          },
-          {
-            image: 'static/img/team-logo/Sunrisers_Hyderabad.png',
-            name: 'Sunrisers Hyderabad',
-            photo: 'static/img/team-photo/Sunrisers_Hyderabad.jpg',
-            short: 'SH',
-            width: '150px',
-            marginTop: '0px',
-            color: 'linear-gradient( 135deg, #FD6E6AF1 10%, #FFC600C1 100%)'
-          }
-        ]
-      };
+    props: {
+      team: {
+        type: Object,
+        required: true
+      }
     },
     methods: {
       style(photo) {
         return `background: url(${photo}) center center no-repeat`;
       }
-    },
+    }
   };
 </script>
 
 <style lang="scss" scoped>
-  .team-list{
+
+  %team-card {
+    width: 316px;
+    height: 381px;
+    border-radius: 5px;
+    display: inline-block;
+    cursor: pointer;
+    font-family: 'IBM Plex Sans', sans-serif;
+    margin: 0 10px 0 0;
+    background-size: cover;
+    color: #fff;
+    text-align: center;
+    text-rendering: optimizeLegibility;
     overflow: hidden;
-    float: left;
-    .team-card {
-      width: 316px;
-      height: 381px;
-      border-radius: 5px;
-      display: inline-block;
-      cursor: pointer;
-      font-family: 'IBM Plex Sans', sans-serif;
-      margin: 0 10px 0 0;
-      background-size: cover;
-      color: #fff;
-      text-align: center;
-      text-rendering: optimizeLegibility;
-      overflow: hidden;
-      & .content  {
-        overflow: hidden;
-        height: 100%;
-        z-index: 2;
-        & .logo {
-          height: 101px;
-          padding: 13px;
-          object-fit: cover;
+    transition: all 0.5s ease-in-out;
+    perspective: 500px;
+  }
+  .team-card{
+    @extend %team-card;
+
+    &:hover .card {
+      transform: rotateY(180deg);
+    }
+    & .card {
+      position: relative;
+      transform-style: preserve-3d;
+      transition: .5s ease;
+
+      & .team-card-front {
+        width: 316px;
+        height: 381px;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        backface-visibility: hidden;
+        & .content  {
+          overflow: hidden;
+          height: 100%;
+          z-index: 2;
+          & .logo {
+            height: 101px;
+            padding: 13px;
+            object-fit: cover;
+          }
+          & .text{
+            & .team-name{
+              font-family: 'IBM Plex Sans', sans-serif;
+              font-size: 25px;
+              font-weight: 700;
+              color: #ffffff;
+              white-space: initial;
+              width: 100%;
+              padding: 20px;
+            }
+          }
         }
-        & .text{
-          & .team-name{
+      }
+      & .team-card-back {
+        width: 316px;
+        height: 381px;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        backface-visibility: hidden;
+        transform: rotateY(180deg);
+        & .content {
+          overflow: hidden;
+          height: 100%;
+          z-index: 2;
+          & .team-statistic{
             font-family: 'IBM Plex Sans', sans-serif;
             font-size: 25px;
             font-weight: 700;
-            color: #ffffff;
-            white-space: initial;
-            width: 100%;
-            padding: 20px;
+            color:white;
           }
         }
       }
